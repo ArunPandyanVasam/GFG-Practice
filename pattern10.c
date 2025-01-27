fdcc49f97ff49670d49b79c2b3d1bb82640edeb1
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-01-27 17:42:15 -0500

    pattern10

---
+++ pattern10.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+
+void pattern9(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        //  space
+        for (int j = 0; j < i; j++)
+        {
+            printf("-");
+        }
+
+        //  star
+        for (int j = 0; j < n - i; j++)
+        {
+            printf("* ");
+        }
+
+        puts(" ");
+    }
+}
+
+int main()
+{
+    puts("\n");
+
+    int n;
+    printf("Input: ");
+    scanf("%d", &n);
+    pattern9(n);
+
+    puts("\n");
+    return 0;
+}
