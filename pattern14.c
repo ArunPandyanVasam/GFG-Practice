854a9b6cebc1bec86ea9fdd4e4f6c57face7d5a3
ArunPandyanVasam <vasamarunpandyan@gmail.com>
2025-01-28 16:35:41 -0500

    pattern14

---
+++ pattern14.c
@@ -0,0 +1,42 @@
+
+#include <stdio.h>
+
+void pattern13(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        //  space
+        for (int j = 0; j < i; j++)
+        {
+            printf("-");
+        }
+
+        //  star
+        for (int j = 1; j <= 2 * n - (i + i + 1); j++)
+        {
+            if (j == 1 || j == 2 * n - (i + i + 1) || i == 0)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf("-");
+            }
+        }
+
+        puts(" ");
+    }
+}
+
+int main()
+{
+    puts("\n");
+
+    int n;
+    printf("Input: ");
+    scanf("%d", &n);
+    pattern13(n);
+
+    puts("\n");
+    return 0;
+}
